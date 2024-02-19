5302764b31e54c8cd12546520d7cdac925d4283b
Prayash Shrestha <xrestha17@gmail.com>
2024-02-19 09:53:58 -0600

    completed loops and arrays exercise

--- 04_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercise01_StoreOrders.java
+++ 04_Loops_and_Arrays/exercise/src/main/java/com/techelevator/Exercise01_StoreOrders.java
@@ -3,16 +3,16 @@ package com.techelevator;
 public class Exercise01_StoreOrders {
 
     /*
-    Sally's Pizza is bringing its pizza ordering system into the digital age
-    to get better customer insights.
-
-    They've encoded each of Sally's pizzas and other dishes as an integer:
-	 * 10: small, cheese       20: medium, cheese       30: large, cheese
-	 * 11: small, pepperoni    21: medium, pepperoni    31: large, pepperoni
-	 * ---
-	 * 40: calzone
-	 * 41: spaghetti pie
-	 * 42: baked ziti
+     * Sally's Pizza is bringing its pizza ordering system into the digital age
+     * to get better customer insights.
+     * 
+     * They've encoded each of Sally's pizzas and other dishes as an integer:
+     * 10: small, cheese 20: medium, cheese 30: large, cheese
+     * 11: small, pepperoni 21: medium, pepperoni 31: large, pepperoni
+     * ---
+     * 40: calzone
+     * 41: spaghetti pie
+     * 42: baked ziti
      */
 
     // You can use these constants in your solutions.
@@ -30,55 +30,84 @@ public class Exercise01_StoreOrders {
     private final int BAKED_ZITI = 42;
 
     /*
-    Each customer order, consisting of one or more items, is represented as an array
-    where each value represents an item in that order.
-
-    Create an "order" that contains the following items:
-	 * - small, cheese (SMALL_CHEESE)
-	 * - calzone (CALZONE)
-	 * - large, pepperoni (LARGE_PEPPERONI)
-	 * - spaghetti pie (SPAGHETTI_PIE)
-
-	 Examples:
-	 createOrder() → [10, 40, 31, 41]
+     * Each customer order, consisting of one or more items, is represented as an
+     * array
+     * where each value represents an item in that order.
+     * 
+     * Create an "order" that contains the following items:
+     * - small, cheese (SMALL_CHEESE)
+     * - calzone (CALZONE)
+     * - large, pepperoni (LARGE_PEPPERONI)
+     * - spaghetti pie (SPAGHETTI_PIE)
+     * 
+     * Examples:
+     * createOrder() → [10, 40, 31, 41]
      */
     public int[] createOrder() {
-        return new int[] {};
+        int[] orders = new int[4];
+        orders[0] = 10;
+        orders[1] = 40;
+        orders[2] = 31;
+        orders[3] = 41;
+        return orders;
     }
 
     /*
-    Sally realized that she needed to know how many calzones her shop sells per day.
-
-    Implement the logic to count the number of calzones sold per day when given an
-    array representing each item that her customers ordered that day.
-
-    Examples:
-    getCalzoneSales([CALZONE, LARGE_CHEESE, LARGE_PEPPERONI, CALZONE, SMALL_CHEESE]) → 2
-    which is the same as:
-    getCalzoneSales([40, 30, 31, 40, 10]) → 2
-
-    getCalzoneSales([LARGE_CHEESE, LARGE_PEPPERONI, SMALL_CHEESE]) → 0
-    getCalzoneSales([]) → 0
+     * Sally realized that she needed to know how many calzones her shop sells per
+     * day.
+     * 
+     * Implement the logic to count the number of calzones sold per day when given
+     * an
+     * array representing each item that her customers ordered that day.
+     * 
+     * Examples:
+     * getCalzoneSales([CALZONE, LARGE_CHEESE, LARGE_PEPPERONI, CALZONE,
+     * SMALL_CHEESE]) → 2
+     * which is the same as:
+     * getCalzoneSales([40, 30, 31, 40, 10]) → 2
+     * 
+     * getCalzoneSales([LARGE_CHEESE, LARGE_PEPPERONI, SMALL_CHEESE]) → 0
+     * getCalzoneSales([]) → 0
      */
     public int getCalzoneSales(int[] orders) {
-        return 0;
+        int calzonesCount = 0;
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == CALZONE) {
+                calzonesCount++;
+            }
+        }
+        return calzonesCount;
     }
 
     /*
-    Sally also needs to know the total revenue for all cheese pizzas sold on any given day.
-        * Each small cheese pizza costs $8.
-        * Each medium cheese pizza costs $11.
-        * Each large cheese pizza costs $14.
-
-    Implement the logic to return the total revenue of all cheese pizzas when given
-    an array representing each item that her customers ordered that day.
-
-    Examples:
-    getCheesePizzaRevenue([SMALL_CHEESE]) → 8
-    getCheesePizzaRevenue([SMALL_CHEESE, SMALL_PEPPERONI, MEDIUM_CHEESE]) → 19
-    getCheesePizzaRevenue([SMALL_PEPPERONI, MEDIUM_PEPPERONI]) → 0
+     * Sally also needs to know the total revenue for all cheese pizzas sold on any
+     * given day.
+     * Each small cheese pizza costs $8.
+     * Each medium cheese pizza costs $11.
+     * Each large cheese pizza costs $14.
+     * 
+     * Implement the logic to return the total revenue of all cheese pizzas when
+     * given
+     * an array representing each item that her customers ordered that day.
+     * 
+     * Examples:
+     * getCheesePizzaRevenue([SMALL_CHEESE]) → 8
+     * getCheesePizzaRevenue([SMALL_CHEESE, SMALL_PEPPERONI, MEDIUM_CHEESE]) → 19
+     * getCheesePizzaRevenue([SMALL_PEPPERONI, MEDIUM_PEPPERONI]) → 0
      */
     public int getCheesePizzaRevenue(int[] orders) {
-        return 0;
+        int amountForCheesePizza = 0;
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == SMALL_CHEESE) {
+                amountForCheesePizza = amountForCheesePizza + 8;
+            }
+            if (orders[i] == MEDIUM_CHEESE) {
+                amountForCheesePizza = amountForCheesePizza + 11;
+            }
+            if (orders[i] == LARGE_CHEESE) {
+                amountForCheesePizza = amountForCheesePizza + 14;
+            }
+        }
+        return amountForCheesePizza;
     }
 }
