3e06fe192fa8e50118ca70360eb3cb016843f1bb
Prayash Shrestha <xrestha17@gmail.com>
2024-03-04 12:00:00 -0600

    completed collections part 2 tutorial

--- 08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ 08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -4,17 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tutorial {
-	
 
     public static void main(String[] args) {
 
         // Step One: Declare a Map
+        Map<String, String> studentResults = new HashMap<String, String>();
 
-    	
-    	// Step Two: Add items to a Map
+        // Step Two: Add items to a Map
+        studentResults.put("Prayash", "Passed");
+        studentResults.put("Mark", "Failed");
 
-    	
-    	// Step Three: Loop through a Map
+        // Step Three: Loop through a Map
+        for (Map.Entry<String, String> result : studentResults.entrySet()) {
+            System.out.println(result.getKey() + " has " + result.getValue() + " the exam.");
+        }
 
     }
 
