521cf31defe9fcf352d08ee9a79304c7d25b7140
Prayash Shrestha <xrestha17@gmail.com>
2024-03-17 17:27:59 -0500

    completed week 5 classes and encapsulation and tutorial

--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,5 +1,46 @@
 package com.techelevator;
 
 public class Elevator {
+    private int currentFloor;
 
+    private int numberOfFloors;
+
+    private boolean doorOpen;
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
+
+    public Elevator(int numberOfLevels) {
+        this.numberOfFloors = numberOfLevels;
+        this.currentFloor = 1;
+    }
+
+    public void openDoor() {
+        doorOpen = true;
+    }
+
+    public void closeDoor() {
+        doorOpen = false;
+    }
+
+    public void goUp(int desiredFloor) {
+        if (!doorOpen && desiredFloor > currentFloor && desiredFloor <= numberOfFloors) {
+            currentFloor = desiredFloor;
+        }
+    }
+
+    public void goDown(int desiredFloor) {
+        if (!doorOpen && desiredFloor < currentFloor && desiredFloor >= 1) {
+            currentFloor = desiredFloor;
+        }
+    }
 }
--- 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ 09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,5 +1,39 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    private int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
 
+    private String letterGrade;
+
+    public HomeworkAssignment(int possibleMarks, String submitterName) {
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
+    }
+
+    public int getEarnedMarks() {
+        return earnedMarks;
+    }
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+
+    public String getLetterGrade() {
+        int percentage = (100 * earnedMarks) / possibleMarks;
+        if (percentage >= 90) return "A";
+        if (percentage >= 80) return "B";
+        if (percentage >= 70) return "C";
+        if (percentage >= 60) return "D";
+        return "F";
+    }
+
+    public void setEarnedMarks(int earnedMarks) {
+        this.earnedMarks = earnedMarks;
+    }
 }
